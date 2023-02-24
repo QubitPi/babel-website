02535d98d9d3bfdf820cd6ff65a243b5dbe6d9d6
Jack <jack20191124@proton.me>
2023-02-24 16:38:04 +0800

    Publish forked site (#1)
    * Publish forked site
    
    * Add CI/CD
    
    * Self-review
    
    * Add PR template
    
    * Self-review: remove comments
    
    * Self-review: preserve upstream yarn dep. versions

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -126,12 +126,12 @@ const siteConfig = {
     experimental_faster: true,
   },
   titleDelimiter: "·",
-  baseUrl: "/",
+  baseUrl: "/babel-website",
   favicon: "img/favicon.png",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "throw",
   customFields: {
-    repoUrl: "https://github.com/babel/babel",
+    repoUrl: "https://github.com/QubitPi/babel-website",
     v6Url: "https://v6.babeljs.io/docs/setup/",
     users,
     sponsors,
@@ -268,7 +268,9 @@ const siteConfig = {
   },
   title: "Babel",
   tagline: "The compiler for next generation JavaScript",
-  url: "https://babeljs.io",
+  url: "https://paion-data.github.io/",
+  organizationName: "QubitPi",
+  projectName: "babel-website",
 
   scripts: [
     {
