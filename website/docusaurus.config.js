aa5a0725098b7ba6beed201586481299d45f457f
Jiaqi Liu ｜【烬火】胡桃 <jack20191124@proton.me>
2025-02-04 12:30:44 +0800

    Publish study notes (#3)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -128,7 +128,7 @@ const siteConfig = {
     experimental_faster: true,
   },
   titleDelimiter: "·",
-  baseUrl: "/babel-website",
+  baseUrl: "/",
   favicon: "img/favicon.png",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "throw",
@@ -266,7 +266,7 @@ const siteConfig = {
   },
   title: "Babel",
   tagline: "The compiler for next generation JavaScript",
-  url: "https://paion-data.github.io/",
+  url: "https://babel.qubitpi.org/",
   organizationName: "QubitPi",
   projectName: "babel-website",
 
